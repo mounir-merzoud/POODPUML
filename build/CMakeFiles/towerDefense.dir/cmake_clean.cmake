e8002462a3c24bf95e604383b369837c95c57ca0
jean-renoul <jean.renoul@laplateforme.io>
2024-05-22 18:15:53 +0200

    installed sfml

---
+++ build/CMakeFiles/towerDefense.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/towerDefense.dir/main.cpp.obj"
+  "CMakeFiles/towerDefense.dir/main.cpp.obj.d"
+  "libtowerDefense.dll.a"
+  "towerDefense.exe"
+  "towerDefense.exe.manifest"
+  "towerDefense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/towerDefense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/towerDefense.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for towerDefense.
+# This may be replaced when dependencies are built.
